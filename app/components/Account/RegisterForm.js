1b2acaa4253ad7a5035d98238c1b390dd41ae3e2
ManuFx11 <manuelpaez1994@gmail.com>
2020-07-19 13:19:32 +0200

    Versión Estable Ultima sin Toast

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,35 +1,108 @@
 //Componente de Formulario de Registro
-import React from 'react';
+import React,{useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {Input, Icon, Button} from 'react-native-elements';
 
+//Importo funcion validar email
+import {validarEmail} from '../../utils/validations';
+
+//Importo Loadash
+import {size, isEmpty} from 'lodash';
+
 export default function RegisterForm(){
+
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+    const [formData, setFormData] = useState(defaultFormValue());
+
+    const onSubmit = () => {
+        if (
+          isEmpty(formData.email) ||
+          isEmpty(formData.password) ||
+          isEmpty(formData.repeatPassword)
+        ) {
+          console.log("Debes rellenar todos los campos")
+        } else if (!validarEmail(formData.email)) {
+          console.log("Email no correcto");
+        } else if (formData.password !== formData.repeatPassword) {
+          console.log("Las contraseñas tienen que ser iguales");
+        } else if (size(formData.password) < 6) {
+           console.log(
+            "La contraseña tiene que tener al menos 6 caracteres"
+          );
+        } else {
+          console.log("todo ok");
+        }
+      };
+    
+      const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text });
+      };
+
+
     return(
         <View style={styles.formContainer}>
-           <Input
-             placeholder="Email"
+        <Input
+             placeholder="Correo electronico"
              containerStyle={styles.inputForm}
-           />
-            <Input
-             placeholder="Contraseña"
-             containerStyle={styles.inputForm}
-             password={true}
-             secureTextEntry={true}
-           />
-            <Input
-             placeholder="Repetir Contraseña"
-             containerStyle={styles.inputForm}
-             password={true}
-             secureTextEntry={true}
-           />
-           <Button 
-             title="Unirse"
-             containerStyle={styles.btnContainerRegister}
-             buttonStyle={styles.btnRegister}
-             />
+             onChange={(e) => onChange(e, "email")}
+             rightIcon={
+                    <Icon
+                    type="material-community"
+                    name="at"
+                    iconStyle={styles.iconRight}
+            />
+        }
+      />
+      <Input
+        placeholder="Contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={showPassword ? false : true}
+        onChange={(e) => onChange(e, "password")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowPassword(!showPassword)}
+          />
+        }
+      />
+      <Input
+        placeholder="Repetir contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={showRepeatPassword ? false : true}
+        onChange={(e) => onChange(e, "repeatPassword")}
+        rightIcon={
+          <Icon
+            type="material-community"
+            name={showRepeatPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.iconRight}
+            onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+          />
+        }
+      />
+      <Button
+        title="Unirse"
+        containerStyle={styles.btnContainerRegister}
+        buttonStyle={styles.btnRegister}
+        onPress={onSubmit}
+      />
         </View>
     )
 }
+
+
+function defaultFormValue() {
+    return {
+      email: "",
+      password: "",
+      repeatPassword: "",
+    };
+  }
 
 const styles = StyleSheet.create({
     
@@ -57,5 +130,8 @@ const styles = StyleSheet.create({
     },
     btnRegister:{
         backgroundColor:"#00A680"
-    }
+    },
+    iconRight: {
+        color: "#c1c1c1",
+      },
 })
---
+++ app/utils/validations.js
@@ -0,0 +1,7 @@
+//Funciones para validaciones no exporto ninguna como default ya que usare varias validaciones que iran en este fichero
+
+//Comprobación para validar email
+export function validarEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
