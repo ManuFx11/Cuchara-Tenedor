65b9681a98f828d1b81c8ae5cb3a6f3620feed8e
ManuFx11 <manuelpaez1994@gmail.com>
2020-08-14 10:28:24 +0200

    Carrusel

---
+++ app/components/Carousel.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Image} from 'react-native-elements';
+//Importo Carrousel
+import Carousel from 'react-native-snap-carousel';
+
+export default function CarouselImages(props) {
+
+    const {arrayImages, height, width} = props;
+
+    //Carousel es un bucle por cada imagen que obtengo haciendo destructurin.
+    const renderItem = ({item}) => {
+        return(
+            <Image
+                style={{width,height}}
+                source={ {uri : item} }
+            />
+        )
+    }
+    
+    return (
+       <Carousel
+        layout={"default"}
+        data={arrayImages}
+        sliderWidth={width}
+        itemWidth={width}
+        renderItem={renderItem}
+        
+       />
+    )
+}
--- app/views/Restaurant/Restaurant.js
+++ app/views/Restaurant/Restaurant.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import React, {useState, useEffect, useCallback} from 'react'
+import { StyleSheet, Text, View, ScrollView, Dimensions, AsyncStorage } from 'react-native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import Loading from '../../components/Loading';
+import CarouselImages from "../../components/Carousel";
 
 //Importo Configuración de Firebase
 import {firebaseApp} from "firebase/app";
@@ -11,6 +12,8 @@ import * as firebase from 'firebase';
 import "firebase/firestore";
 //Creo una conexión a base de datos
 const DB = firebase.firestore(firebaseApp);
+
+const WidthScreen = Dimensions.get("window").width;
 
 export default function Restaurant(props) {
 
@@ -24,31 +27,48 @@ export default function Restaurant(props) {
     //STATE
     const [restaurant, setRestaurant] = useState(null);
     const [isVisible, setIsVisible] = useState(true);
+    console.log(restaurant);
    
 
    //USEEFFECT
+   
    useEffect(() => {
-      DB.collection("restaurants")
-        .doc(id)
-        .get()
-        .then((response) => {
-            setIsVisible(false);
-            //Obtengo la información del restaurante con response.data()
-            const data = response.data();
-            data.id = response.id;
-            //Añado el id y lo añado al estado
-            setRestaurant(data);
-        } )
+       
+     getInfoRestaurant().then((response) => {
+         console.log(response);
+         setRestaurant(response);
+         setIsVisible(false);
+     })
+
    }, [])
 
+  
+   const getInfoRestaurant = async () => {
+        const search = DB.collection("restaurants").doc(id);
+        const result = await search.get();
+        return result.data();
+   }
+
     return (
-        <View>
-            <Text></Text>
+        <ScrollView vertical style={styles.viewBody}>
+         
+           <CarouselImages
+                 arrayImages={restaurant.images}
+                 height={250}
+                 width={WidthScreen}
+             />  
             <Loading isVisible={isVisible} text="Cargando Restaurante"/>
-        </View>
+        </ScrollView>
       
         
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    viewBody : {
+        flex:1,
+        backgroundColor:"#FFF"
+    }
+
+})
